573f6ab3ad15715b6a3757501b51521b02001cf0
Mickael <60462022+MickaelMedia@users.noreply.github.com>
2021-04-20 17:17:27 -0400

    side project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,60 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { createFFmpeg, fetchFile} from "@ffmpeg/ffmpeg";
+const ffmpeg= createFFmpeg({ log:true}); 
+
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
+
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState();
+  const [gif, setGif] = useState();
+  const [time, setTime] =useState();
+  const [duration, setDuration] = useState();
+
+  const load = async ()=>{
+    await ffmpeg.load();
+    setReady(true);
+  }
+
+  useEffect(() =>{
+    load();
+  }, [])
+
+  const convertToGif = async() =>{
+    ffmpeg.FS("writeFile", "test.mp4", await fetchFile(video));
+
+    await ffmpeg.run("-i", "test.mp4", "-t", duration, "-ss", time, "-f", "gif", "out.gif");
+    // -t stands for time of the gif and -ss stands for delay.
+
+    const data = ffmpeg.FS("readFile", "out.gif");
+
+    const url = URL.createObjectURL(new Blob([data.buffer], { type: "image/gif"}));
+    setGif(url)
+  }
+
+  return ready ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      {video && <video controls width="250" src={URL.createObjectURL(video)}></video>} <br/>
+      
+       <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))}/>
+       <br />
+       <p>Delay (time to start at)</p><input type="text" onChange={(e) => setTime(e.target.value)}/>
+       <br />
+       <p>Duration </p><input type="text" onChange={(e) => setDuration(e.target.value)}/>
+      <p>the duration is {duration} and delay is {time}</p>
+       <h3>Result </h3>
+
+       <button onClick={convertToGif}>Convert</button> <br/>
+
+       {gif &&<img src={gif} width="250"/>}
+
+       <p>For video reference check out :</p>
+       <p><a href="https://www.youtube.com/watch?v=-OTc0Ki7Sv0&list=WL&index=9">https://www.youtube.com/watch?v=-OTc0Ki7Sv0&list=WL&index=9</a></p>
+
     </div>
-  );
+  ) :
+  (<p>Loading ...</p>);
 }
 
 export default App;
